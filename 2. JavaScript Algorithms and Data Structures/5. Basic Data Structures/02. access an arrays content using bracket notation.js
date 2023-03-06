bafbf5ad12e781a487fbc2304469245eccf3f06f
deepakmardii <deepakmardi22@gmail.com>
2023-03-06 20:12:20 +0530

    commit commit

---
+++ 2. JavaScript Algorithms and Data Structures/5. Basic Data Structures/02. access an arrays content using bracket notation.js
@@ -0,0 +1,7 @@
+//access-an-arrays-contents-using-bracket-notation
+
+let myArray = ["a", "b", "c", "d"];
+// change code below this line
+    myArray[1] = "Hello";
+//change code above this line
+console.log(myArray);
