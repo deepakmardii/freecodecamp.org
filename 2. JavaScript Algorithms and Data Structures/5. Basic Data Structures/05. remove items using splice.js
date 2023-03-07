90a2ca99acb48d884a0aeb56e96d983b0a1e617a
deepakmardii <deepakmardi22@gmail.com>
2023-03-07 20:12:20 +0530

    commit commit

---
+++ 2. JavaScript Algorithms and Data Structures/5. Basic Data Structures/05. remove items using splice.js
@@ -0,0 +1,11 @@
+//remove-items-using-splice
+//Remember 0 Based Indexing........
+
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// only change code below this line
+
+const newArr = arr.splice(1, 4);
+
+// only change code above this line
+console.log(arr);
